410691880f2ac4f0244e9dd6483a2d89ad8f9e23
Ophelia0631 <1216733498@qq.com>
2025-07-20 14:50:14 +0800

    Add files via upload

---
+++ LED/Core/Src/main.c
@@ -0,0 +1,78 @@
+#include "main.h"
+#include "stm32f1xx_hal_tim.h"
+
+
+
+void Error_Handler(void)
+{
+    while (1)
+    {
+    }
+}
+
+void SystemClock_Config(void)
+{
+    RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+    RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+    RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+    RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource=RCC_PLLSOURCE_HSE;
+    RCC_OscInitStruct.PLL.PLLMUL=RCC_PLL_MUL9;
+    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+    {
+        Error_Handler();
+	}
+	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK|RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+	{
+		Error_Handler();
+	}
+}//时钟设置
+
+void gpio_init(void)
+{
+	GPIO_InitTypeDef gpio_initstruct={0};
+	gpio_initstruct.Pin=GPIO_PIN_0;
+	gpio_initstruct.Speed=GPIO_SPEED_FREQ_HIGH;
+	gpio_initstruct.Pull=GPIO_NOPULL;
+	gpio_initstruct.Mode=GPIO_MODE_OUTPUT_PP;
+	__HAL_RCC_GPIOA_CLK_ENABLE();
+	HAL_GPIO_Init(GPIOA,&gpio_initstruct);
+}//引脚设置
+
+
+
+int main(void)
+{
+    HAL_Init();
+    SystemClock_Config();
+	HAL_NVIC_SetPriorityGrouping(NVIC_PRIORITYGROUP_2);
+	gpio_init();
+
+    while (1)
+    {
+		HAL_GPIO_TogglePin(GPIOA,GPIO_PIN_0);
+		HAL_Delay(500);
+    }
+}
+
+
+
+
+
+
+#ifdef  USE_FULL_ASSERT
+
+void assert_failed(uint8_t *file, uint32_t line)
+{
+ 
+}
+#endif
